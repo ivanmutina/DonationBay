d1b0eb5f0e5cbaeaa3fde020e6f04786a594c1e3
Ivan <43207364+ivanmutina@users.noreply.github.com>
2022-08-03 01:09:17 +0200

    Landing page design

---
+++ src/components/cardComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card text-center">
+    <div class="card-header">Naslov</div>
+    <div class="card-body">
+      <img :src="info" alt="" />
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repudiandae consectetur placeat quisquam adipisci similique, quia tenetur doloremque sequi minima officiis sapiente ullam voluptatibus eius illo cum dolores et
+        autem.
+        <a href="#" class="btn btn-primary mt-2">Doniraj!</a>
+      </li>
+    </ul>
+    <div class="card-footer text-muted">2 days ago</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["info"],
+  name: "cardComp",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,42 @@
-<template border>
-  <div class="row align-items-center mt-5">
-    <div class="col">
-      <div class="card text-center">
-        <div class="card-header">Featured</div>
-        <div class="card-body">
-          <img src="https://picsum.photos/600" alt="" />
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repudiandae consectetur placeat quisquam adipisci similique, quia tenetur doloremque sequi minima officiis sapiente ullam voluptatibus eius illo cum
-            dolores et autem.
-            <a href="#" class="btn btn-primary mt-2">Doniraj!</a>
-          </li>
-        </ul>
-        <div class="card-footer text-muted">2 days ago</div>
+<template>
+  <div class="container">
+    <div class="row align-items-center mt-5">
+      <div class="col">
+        <img class="w-100 h-75 hover-shadow" src="@/assets/donate2.jpg" alt="" />
+      </div>
+      <div class="col border">
+        <h1 display-1>Donation Bay</h1>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo est doloribus illum quae tempore consectetur temporibus molestias assumenda quaerat sapiente fugit ducimus aspernatur perferendis, deleniti quidem
+          molestiae, cum dicta iste.
+        </h3>
       </div>
     </div>
-    <div class="col">
-      <div class="card text-center">
-        <div class="card-header">Featured</div>
-
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repudiandae consectetur placeat quisquam adipisci similique, quia tenetur doloremque sequi minima officiis sapiente ullam voluptatibus eius illo cum
-            dolores et autem.
-            <a href="#" class="btn btn-primary mt-2">Doniraj!</a>
-          </li>
-        </ul>
-        <div class="card-footer text-muted">2 days ago</div>
-      </div>
+    <div class="row align-items-center mt-5">
+      <!-- -->
+      <div class="col border"><card-comp v-for="card in cards" :key="card" :info="card" /></div>
+      <div class="col border"></div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import cardComp from "@/components/cardComp.vue";
+
+let cards = [];
+
+cards = ["https://picsum.photos/id/1/600", "https://picsum.photos/id/2/600", "https://picsum.photos/id/3/600"];
 
 export default {
   name: "Home",
+  data: () => {
+    return {
+      cards: cards,
+    };
+  },
   components: {
-    HelloWorld,
+    cardComp,
   },
 };
 </script>
